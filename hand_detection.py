9690ff03f060976ce9239b4a953820e219aa18cc
Otto02li <1321117335@qq.com>
2022-08-05 18:32:10 +0800

    增加实时显示FPS的功能

--- hand_detection.py
+++ hand_detection.py
@@ -1,6 +1,9 @@
 import cv2
 import mediapipe as mp
+import time
 
+pTime = 0   # previoustime之前的时间
+cTime = 0   # currenttime当前的时间
 # 新建窗口
 cv2.namedWindow('video', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('video', 640, 480)
@@ -19,6 +22,7 @@ while True:
         break
     # 读到图像
     # bgr 转 rgb给mediapipe用
+    frame = cv2.flip(frame, 1)  # 镜像操作
     imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     result = hands.process(imgRGB)
     # print(result.multi_hand_landmarks)
@@ -31,9 +35,17 @@ while True:
                 xPos = int(lm.x * 640)       # lm.x,lm.y是对应点x,y坐标占窗口的比例，乘回窗口长宽得到点的坐标
                 yPos = int(lm.y * 480)
                 # 在图上标出点的下标
+                if i == 12:
+                    cv2.circle(frame, (xPos, yPos), 20, (5, 96, 36), cv2.FILLED)
+                #  在图像上显示各点的下标
                 cv2.putText(frame, str(i), (xPos-25, yPos+5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 1)
                 print(i, xPos, yPos)
-    #frame = cv2.flip(frame, 1)  # 镜像操作
+
+    # 计算FPS
+    cTime = time.time()     # cTime=现在的时间
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(frame, f"FPS    :   {int(fps)}", (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 3)
     cv2.imshow('video', frame)
 
     key = cv2.waitKey(1)
